17e0e9b45931fe929c4e5a24259f6311eb4ab1dd
Shahlal47 <shahlalhossain@gmail.com>
2014-12-26 01:35:23 +0600

    update with all field

--- personalinfo/update.php
+++ personalinfo/update.php
@@ -1,7 +1,79 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shahlal
- * Date: 25/12/14
- * Time: 22:44
- */ 
+//print_r($_POST);
+
+$id = $_POST['id'];
+$code = $_POST['code'];
+$track = $_POST['track'];
+$fullname = $_POST['fullname'];
+$fathername = $_POST['fathername'];
+$mothername = $_POST['mothername'];
+$religion = $_POST['religion'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$nationality = $_POST['nationality'];
+$nationalid = $_POST['nationalid'];
+$useremail = $_POST['useremail'];
+$mobile = $_POST['mobile'];
+$hometown = $_POST['hometown'];
+$currentcity = $_POST['currentcity'];
+$sscroll = $_POST['sscroll'];
+$sscboard = $_POST['sscboard'];
+$sscyear = $_POST['sscyear'];
+$sscgroup = $_POST['sscgroup'];
+$sscresult = $_POST['sscresult'];
+$hscroll = $_POST['hscroll'];
+$hscboard = $_POST['hscboard'];
+$hscyear = $_POST['hscyear'];
+$hscgroup = $_POST['hscgroup'];
+$hscresult = $_POST['hscresult'];
+$honssubject = $_POST['honssubject'];
+$honsresult = $_POST['honsresult'];
+$bangla = $_POST['bangla'];
+$english = $_POST['english'];
+
+
+$link = mysqli_connect("localhost", "root", "479874", "crud01");
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+$query = "UPDATE `crud01`.`personalinfo` SET `code` = '".$code."',
+                                            `track` = '".$track."',
+                                             `fullname` = '".$fullname."',
+                                              `fathername` = '".$fathername."',
+                                               `mothername` = '".$mothername."',
+                                                `religion` = '".$religion."',
+                                                 `dob` = '".$dob."',
+                                                  `gender` = '".$gender."',
+                                                   `nationality` = '".$nationality."',
+                                                    `nationalid` = '".$nationalid."',
+                                                     `useremail` = '".$useremail."',
+                                                      `mobile` = '".$mobile."',
+                                                       `hometown` = '".$hometown."',
+                                                        `currentcity` = '".$currentcity."',
+                                                         `sscroll` = '".$sscroll."',
+                                                          `sscboard` = '".$sscboard."',
+                                                           `sscyear` = '".$sscyear."',
+                                                            `sscgroup` = '".$sscgroup."',
+                                                             `sscresult` = '".$sscresult."',
+                                                              `hscroll` = '".$hscroll."',
+                                                               `hscboard` = '".$hscboard."',
+                                                                `hscyear` = '".$hscyear."',
+                                                                 `hscgroup` = '".$hscgroup."',
+                                                                  `hscresult` = '".$hscresult."',
+                                                                   `honssubject` = '".$honssubject."',
+                                                                    `honsresult` = '".$honsresult."',
+                                                                     `bangla` = '".$bangla."',
+                                                                      `english` = '".$english."'
+            WHERE `personalinfo`.`id` = $id;";
+mysqli_query($link, $query);
+
+
+//$query = "UPDATE `students`.`users` SET `first_name` = '".$firstName."',
+//`last_name` = '".$lastName ."' WHERE `users`.`id` = $id;";
+
+header('location:list.php');
+?>
