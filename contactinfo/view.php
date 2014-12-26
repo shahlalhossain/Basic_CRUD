e1384dce7027a0a18c7ee550c15b7f98a68e5802
Shahlal47 <shahlalhossain@gmail.com>
2014-12-26 09:21:17 +0600

    view.php file is working properly

--- contactinfo/view.php
+++ contactinfo/view.php
@@ -1,11 +1,12 @@
 <?php
 $id = $_GET['id'];
 $link = mysqli_connect("localhost", "root", "lict@2", "crud01");
-$query = "SELECT * FROM personalinfo WHERE id = $id";
+$query = "SELECT * FROM contactinfo WHERE id = $id";
 $result = mysqli_query($link, $query);
 $row = mysqli_fetch_assoc($result);
 //print_r($row);
 ?>
+
 <html>
 <head>
     <title></title>
@@ -13,40 +14,18 @@ $row = mysqli_fetch_assoc($result);
 <body style="text-align: center;">
 <ul>
     <li><a href="create.php">Create New Record</a> </li>
-    <li><a href="list.php">View All Participant</a> </li>
-    <li><a href="search.php">Search By ID</a> </li>
 </ul>
-<table border="1">
+<table border="1" width="100%">
     <tr>
         <td>ID</td>
-        <td>Code</td>
-        <td>Track</td>
-        <td>Name</td>
-        <td>F. Name</td>
-        <td>M. Name</td>
-        <td>Religion</td>
-        <td>DOB</td>
-        <td>Gender</td>
-        <td>Nationality</td>
-        <td>National ID</td>
-        <td>Email</td>
+        <td>Present Address</td>
+        <td>Permanent address</td>
+        <td>District</td>
+        <td>Home Phone</td>
         <td>Mobile</td>
-        <td>Home Town</td>
-        <td>Current City</td>
-        <td>S.S.C Roll</td>
-        <td>S.S.C Board</td>
-        <td>S.S.C Passing Year</td>
-        <td>S.S.C Group</td>
-        <td>S.S.C Result</td>
-        <td>H.S.C Roll</td>
-        <td>H.S.C Board</td>
-        <td>H.S.C Passing Year</td>
-        <td>H.S.C Group</td>
-        <td>H.S.C Result</td>
-        <td>Honers Subject</td>
-        <td>Honers Result</td>
-        <td>Bangla Proficiency</td>
-        <td>English Proficiency</td>
+        <td>Emergency Contact</td>
+        <td>Email</td>
+        <td>Alternative Email</td>
         <td>Action</td>
     </tr>
     <?php
@@ -54,36 +33,15 @@ $row = mysqli_fetch_assoc($result);
         ?>
         <tr>
             <td><?php echo $row['id']?></td>
-            <td><?php echo $row['code']?></td>
-            <td><?php echo $row['track']?></td>
-            <td><?php echo $row['fullname']?></td>
-            <td><?php echo $row['fathername']?></td>
-            <td><?php echo $row['mothername']?></td>
-            <td><?php echo $row['religion']?></td>
-            <td><?php echo $row['dob']?></td>
-            <td><?php echo $row['gender']?></td>
-            <td><?php echo $row['nationality']?></td>
-            <td><?php echo $row['nationalid']?></td>
-            <td><?php echo $row['useremail']?></td>
+            <td><?php echo $row['present_address']?></td>
+            <td><?php echo $row['permanent_address']?></td>
+            <td><?php echo $row['district']?></td>
+            <td><?php echo $row['home_phone']?></td>
             <td><?php echo $row['mobile']?></td>
-            <td><?php echo $row['hometown']?></td>
-            <td><?php echo $row['currentcity']?></td>
-            <td><?php echo $row['sscroll']?></td>
-            <td><?php echo $row['sscboard']?></td>
-            <td><?php echo $row['sscyear']?></td>
-            <td><?php echo $row['sscgroup']?></td>
-            <td><?php echo $row['sscresult']?></td>
-            <td><?php echo $row['hscroll']?></td>
-            <td><?php echo $row['hscboard']?></td>
-            <td><?php echo $row['hscyear']?></td>
-            <td><?php echo $row['hscgroup']?></td>
-            <td><?php echo $row['hscresult']?></td>
-            <td><?php echo $row['honssubject']?></td>
-            <td><?php echo $row['honsresult']?></td>
-            <td><?php echo $row['bangla']?></td>
-            <td><?php echo $row['english']?></td>
-            <td> <a href="edit.php?id=<?php echo $row['id']?>">Edit</a> |
-                 <a href="delete.php?id=<?php echo $row['id']?>">Delete</a></td>
+            <td><?php echo $row['emergency_contact']?></td>
+            <td><?php echo $row['email']?></td>
+            <td><?php echo $row['alternative_email']?></td>
+            <td> <a href="edit.php?id=<?php echo $row['id']?>">Edit</a> | <a href="delete.php?id=<?php echo $row['id']?>">Delete</a> |
         </tr>
     <?php
     }
