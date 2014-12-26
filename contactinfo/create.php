c0bf4b8d9f441d12aa3d79cc63a22deca77e4342
Shahlal47 <shahlalhossain@gmail.com>
2014-12-26 08:35:41 +0600

    contactinfo/create.php is working properly

---
+++ contactinfo/create.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body style="text-align: center; width: 100%; height: 100%;">
+<form action="add.php" method="POST">
+
+    <h3>Contact Information</h3>
+
+    <label>Present Adress: </label>
+    <input type="text" name="present_address" />
+    <br>
+    <label>Permanent Address: </label>
+    <input type="text" name="permanent_address">
+    <br>
+    <label>District: </label>
+    <input type="text" name="district" />
+    <br>
+    <label>Home Phone: </label>
+    <input type="text" name="home_phone" />
+    <br>
+    <label>Mobile: </label>
+    <input type="text" name="mobile" />
+    <br>
+    <br>
+    <label>Email: </label>
+    <input type="text" name="email" />
+    <br>
+    <label>Alternate Email: </label>
+    <input type="text" name="alternate_email" />
+    <br>
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
