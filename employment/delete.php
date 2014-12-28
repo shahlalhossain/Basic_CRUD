1a42a165a7b7b87a495282383efbbf7067f7ee3e
Shahlal47 <shahlalhossain@gmail.com>
2014-12-28 13:41:03 +0600

    move some files to a folder

--- employment/delete.php
+++ employment/delete.php
@@ -1,6 +1,10 @@
 <?php
 $id = $_GET['id'];
+<<<<<<< HEAD
+$link = mysqli_connect("localhost", "root", "479874", "crud01");
+=======
 $link = mysqli_connect("localhost", "root", "lict@2", "crud01");
+>>>>>>> dd1c2428e51392082af555555a0479e7fd0c452a
 $query = "DELETE FROM `crud01`.`deploment` WHERE `deploment`.`id` = $id";
 mysqli_query($link, $query);
 header('location:list.php');
